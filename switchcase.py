81e389966611a5ecd4cd6cfa33ef0daff468235f
Samuel O. Ugowe <sdougowe@gmail.com>
2017-01-18 21:26:02 -0500

    Update switchcase.py

--- switchcase.py
+++ switchcase.py
@@ -5,32 +5,34 @@ def switchcase(switch, experiment=0):
     if "case0" == switch or fallthru == True:
         fallthru = True
         #do the thing
-        #print("In case zero")
+        print("In case zero")
     if "case1" == switch or fallthru == True:
         fallthru = True
         #do the thing     
-        #print("In case one")   
+        print("In case one")   
     if "case2" == switch or fallthru == True:
         fallthru = True
         #do the thing
-        #print("In case two")
+        print("In case two")
         return
     if "case3" == switch or fallthru == True:
         fallthru = True
         #do the thing
-        #print("In case three")
+        print("In case three")
         return
     else:
         #do the default thing
         print("Defaulting...")
 
 if __name__ == "__main__":
+    print("Test 1('case0'): ")
     switchcase("case0", 1);
-    print()
+    print("\nTest 2('case1'): ")
     switchcase("case1", 2)
-    print()
+    print("\nTest 3('case2'): ")
     switchcase("case2", 3)
-    print()
+    print("\nTest 4('case3'): ")
     switchcase("case3", 4)
-    print()
+    print("\nTest 5('case27'/default): ")
     switchcase("case27", 5)
+
